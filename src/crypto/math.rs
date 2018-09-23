f56df19775c8f078efdfe0d474e16bc9c41c07d7
Jack Grigg <str4d@i2pmail.org>
2018-09-23 20:23:04 +0100

    Fix crypto::math::rectify logic

--- src/crypto/math.rs
+++ src/crypto/math.rs
@@ -1,30 +1,26 @@
 use num_bigint::BigUint;
 
+/// Converts the given number into an array of exactly len bytes, padding with
+/// zeroes if necessary.
+///
+/// The Java implementation handles the fact that Java BigInteger prepends a
+/// sign bit, which can create an extra leading zero-byte. BigUint does not do
+/// this, so we simplify the logic.
 pub fn rectify(bi: &BigUint, len: usize) -> Vec<u8> {
     let mut b = bi.to_bytes_be();
-    // Pad with 0s if necessary
     match b.len() {
         sz if sz == len => b,
-        sz if sz > (len + 1) => panic!("key too big ({}) max is {}", sz, len + 1),
+        sz if sz > len => panic!("key too big ({}) max is {}", sz, len),
         0 => {
-            println!("Warning: dh_pub is zero!");
+            warn!("Warning: dh_pub is zero!");
             vec![0u8; len]
         }
-        sz if sz > len => match b[0] {
-            0 => Vec::from(&b[1..]),
-            _ => panic!("key too big ({}) max is {}", sz, len),
-        },
         _ => {
-            match b[0] & 0x80 {
-                0 => {
-                    // Smaller than needed
-                    let mut ret = vec![0u8; len];
-                    ret.truncate(len - b.len());
-                    ret.append(&mut b);
-                    ret
-                }
-                _ => panic!("negative"),
-            }
+            // Smaller than needed
+            let mut ret = vec![0u8; len];
+            ret.truncate(len - b.len());
+            ret.append(&mut b);
+            ret
         }
     }
 }
