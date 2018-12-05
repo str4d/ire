02aa22298a5172a3dcec1063f75c693585974e14
Jack Grigg <thestr4d@gmail.com>
2018-12-05 17:16:58 +0000

    [WIP] EncLS2 encryption algorithm

--- src/data/ls2/enc/auth.rs
+++ src/data/ls2/enc/auth.rs
@@ -1,5 +1,19 @@
+use super::Error;
+
 pub(super) const AUTH_ID_LEN: usize = 8;
 pub(super) const AUTH_COOKIE_LEN: usize = 32;
+
+pub struct X25519ClientInfo;
+
+/// Client information
+pub enum ClientInfo {
+    X25519(Vec<X25519ClientInfo>),
+}
+
+// Client's secret authentication key
+pub enum ClientSecretKey {
+    X25519,
+}
 
 /// Per-client authentication data.
 #[derive(Debug, PartialEq)]
@@ -12,4 +26,27 @@ pub(super) struct ClientAuthData {
 #[derive(Debug, PartialEq)]
 pub(super) enum ClientAuthType {
     X25519([u8; 32], Vec<ClientAuthData>),
+}
+
+impl ClientAuthType {
+    pub(super) fn from_info(
+        client_info: Option<ClientInfo>,
+        _subcredential: &[u8],
+        _created: u32,
+    ) -> (Vec<u8>, Option<Self>) {
+        match client_info {
+            Some(ClientInfo::X25519(_clients)) => unimplemented!(),
+            None => (vec![], None),
+        }
+    }
+
+    /// Returns the authCookie if the client is authorized, or an error otherwise.
+    pub(super) fn authenticate(
+        &self,
+        _key: &ClientSecretKey,
+        _subcredential: &[u8],
+        _created: u32,
+    ) -> Result<Vec<u8>, Error> {
+        Err(Error::NotAuthorised)
+    }
 }
